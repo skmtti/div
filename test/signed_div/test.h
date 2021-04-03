ba6fb0889ae94ba0361c0f9ac7a1e28462ac55ef
Nobuaki Sakamoto <sakamoto.rugerman@yahoo.ne.jp>
2021-04-03 23:14:08 +0900

    X value put into dividend and divisor after start div0 exp value change

--- test/signed_div/test.h
+++ test/signed_div/test.h
@@ -1,3 +1,8 @@
+  logic [DATA_WIDTH-1:0] tmp_dividend;
+  logic [DATA_WIDTH-1:0] tmp_divisor;
+  logic [DATA_WIDTH-1:0] exp_quotient;
+  logic [DATA_WIDTH-1:0] exp_remainder;
+
   initial begin
     clk = '0;
     rst_n = '0;
@@ -16,19 +21,35 @@
       @(posedge clk);
       #1;
       {dividend, divisor} = count;
+      {tmp_dividend, tmp_divisor} = {dividend, divisor};
       signed_ope = '1;
+
+      // Div by 0 spec is RISC-V's spec
+      if (divisor == 0) begin
+        exp_quotient = '1;
+        exp_remainder = dividend; 
+      end else begin
+        exp_quotient = $signed(dividend) / $signed(divisor);
+        exp_remainder = $signed(dividend) % $signed(divisor);
+      end
+
       start = 1;
   
       @(posedge clk);
       #1;
       start = 0;
+      {dividend, divisor} = 'x;
   
       @(posedge clk);
       wait(ready);
       @(posedge clk);
-      $display("%d / %d = %d ... %d", $signed(dividend), $signed(divisor), 
-                                      $signed(quotient), $signed(remainder));
-      if ($signed(quotient) != $signed(dividend) / $signed(divisor)) begin
+      $display("%d / %d = %d ... %d (%d ... %d)", 
+            $signed(tmp_dividend), $signed(tmp_divisor), 
+            $signed(quotient), $signed(remainder),
+            $signed(exp_quotient), $signed(exp_remainder));
+
+      if (($signed(quotient)  !== $signed(exp_quotient)) ||
+          ($signed(remainder) !== $signed(exp_remainder))) begin
         $display("NG");
         fail = 1;
       end
--- test/unsigned_div/test.h
+++ test/unsigned_div/test.h
@@ -1,3 +1,8 @@
+  logic [DATA_WIDTH-1:0] tmp_dividend;
+  logic [DATA_WIDTH-1:0] tmp_divisor;
+  logic [DATA_WIDTH-1:0] exp_quotient;
+  logic [DATA_WIDTH-1:0] exp_remainder;
+
   initial begin
     clk = '0;
     rst_n = '0;
@@ -16,18 +21,31 @@
       @(posedge clk);
       #1;
       {dividend, divisor} = count;
+      {tmp_dividend, tmp_divisor} = {dividend, divisor};
       signed_ope = '0;
+      // div by 0 spec is RISC-V spec
+      if (divisor == 0) begin
+        exp_quotient = '1;
+        exp_remainder = dividend; 
+      end else begin
+        exp_quotient = dividend / divisor;
+        exp_remainder = dividend % divisor;
+      end
+
       start = 1;
   
       @(posedge clk);
       #1;
       start = 0;
+      {dividend, divisor} = 'x;
   
       @(posedge clk);
       wait(ready);
       @(posedge clk);
-      $display("%d / %d = %d ... %d", dividend, divisor, quotient, remainder);
-      if (quotient != dividend / divisor) begin
+      $display("%d / %d = %d ... %d (%d ... %d)", 
+                tmp_dividend, tmp_divisor, quotient, remainder, 
+                exp_quotient, exp_remainder);
+      if ((quotient  !== exp_quotient) || (remainder !== exp_remainder)) begin
         $display("NG");
         fail = 1;
       end
